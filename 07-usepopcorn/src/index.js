0365398741ec09d4bb8b9232a90d19c84373b93a
mirkoMac <mirkomimic88@gmail.com>
2025-01-09 08:52:48 +0100

    07-usepopcorn

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./StarRating";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 
 function Test() {
   const [movieRating, setMovieRating] = useState(0);
@@ -18,12 +18,12 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
     <StarRating size={24} color="red" className="test" defaultRating={3} />
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
