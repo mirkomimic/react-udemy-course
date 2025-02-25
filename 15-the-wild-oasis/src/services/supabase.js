da69f843b0967d730f9b75d25c1591f3b1dc7746
mirkoMac <mirkomimic88@gmail.com>
2025-02-25 08:21:22 +0100

    15-the-wild-oasis supabase set up

---
+++ 15-the-wild-oasis/src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://lhkqmjchvldlsqzqyhnp.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxoa3FtamNodmxkbHNxenF5aG5wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAzODA4MzUsImV4cCI6MjA1NTk1NjgzNX0.QP-6OFINuTCTHwcHzDXfNKWjQfV7TqzKe3SXHc-LxT0";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
